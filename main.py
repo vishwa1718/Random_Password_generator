d536dc74896f7866e6853185aecbd48b9452a08a
vishwa1718 <69241781+vishwa1718@users.noreply.github.com>
2021-01-18 18:39:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import string
+import random
+
+
+print("+++ Password Generator+++")
+
+mxchars = string.ascii_letters + string.digits + string.punctuation
+
+numInput = int(input("Enter the Number of passwords to Generate: "))
+length = int(input("Enter the Password Length: "))
+print("+++++++++++++++++++++++++++++++++++++++++++")
+print("+++++++List(s) Generated Password:++++++++ ")
+print("+++++++++++++++++++++++++++++++++++++++++++")
+for pwd in range(numInput):
+    pw = ''
+for c in range(length):
+    pw += random.choice(mxchars)
+    print(pw)
